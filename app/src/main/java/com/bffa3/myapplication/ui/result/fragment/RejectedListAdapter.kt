a62d3ccb5ef8e84bc04152c787c4449ed37894c2
Fikran Akbar <fikranakbar756@gmail.com>
2021-05-23 15:43:07 +0700

    Add dummy item to result activity_result.xml

---
+++ app/src/main/java/com/bffa3/myapplication/ui/result/fragment/RejectedListAdapter.kt
@@ -0,0 +1,35 @@
+package com.bffa3.myapplication.ui.result.fragment
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bffa3.myapplication.data.entity.ResultAcne
+import com.bffa3.myapplication.databinding.ItemResultBinding
+
+class RejectedListAdapter : RecyclerView.Adapter<RejectedListAdapter.CustomViewHolder>() {
+
+    private val resultAcneList = ArrayList<ResultAcne>()
+
+    fun setList(list: List<ResultAcne>) {
+        resultAcneList.clear()
+        resultAcneList.addAll(list)
+    }
+
+    inner class CustomViewHolder(private val binding: ItemResultBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(resultAcne: ResultAcne) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        return CustomViewHolder(ItemResultBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.bind(resultAcneList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return resultAcneList.size
+    }
+}
--- app/src/main/java/com/bffa3/myapplication/ui/result/fragment/RejectedListFragment.kt
+++ app/src/main/java/com/bffa3/myapplication/ui/result/fragment/RejectedListFragment.kt
@@ -5,13 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bffa3.myapplication.R
+import com.bffa3.myapplication.data.entity.ResultAcne
+import com.bffa3.myapplication.databinding.FragmentRejectedListBinding
+import com.bffa3.myapplication.utils.DummyResultAcne
+
 class RejectedListFragment : Fragment() {
+
+    private lateinit var binding: FragmentRejectedListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rejected_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRejectedListBinding.bind(view)
+
+        val adapter = RejectedListAdapter()
+        adapter.setList(DummyResultAcne.addDummyResultAcne())
+        binding.rvRejectedList.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            this.adapter = adapter
+        }
     }
 }
