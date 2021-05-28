b1401bb226f776a0fb705b6b49cf97c8ab6c74b7
Fikran Akbar <fikranakbar756@gmail.com>
2021-05-28 21:33:50 +0700

    fixing bug in ForgotPasswordActivity.kt

--- app/src/main/java/com/b21cap0398/acnescan/ui/forgotpassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/b21cap0398/acnescan/ui/forgotpassword/ForgotPasswordActivity.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import com.b21cap0398.acnescan.R
 import com.b21cap0398.acnescan.databinding.ActivityForgotPasswordBinding
 import com.b21cap0398.acnescan.ui.emailreset.EmailResetActivity
+import com.b21cap0398.acnescan.utils.UserValidationHelper
 import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
@@ -31,15 +32,29 @@ class ForgotPasswordActivity : AppCompatActivity() {
             loadingScreen.visibility = View.VISIBLE
 
             val email = binding.tfEmail.editText?.text.toString()
-            auth.sendPasswordResetEmail(email)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val intent = Intent(this, EmailResetActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                        loadingScreen.visibility = View.VISIBLE
+
+            if (UserValidationHelper.isValidEmail(email)) {
+                binding.tfEmail.isErrorEnabled = false
+                auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val intent = Intent(this, EmailResetActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else {
+                            binding.tfEmail.isErrorEnabled = true
+                            binding.tfEmail.error = getString(R.string.email_not_registered)
+                            loadingScreen.visibility = View.GONE
+                        }
                     }
+            } else {
+                binding.apply {
+                    tfEmail.isErrorEnabled = true
+                    tfEmail.error = getString(R.string.email_is_not_valid)
+                    loadingScreen.visibility = View.GONE
                 }
+            }
         }
     }
 }
